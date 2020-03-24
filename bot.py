9a4a8d07c5bf43d22ac51298d601cf2a43d2f88a
kirill_masanov <kirill_masanov@mail.ru>
2020-03-24 16:15:13 +0300

    Добавлены функции отправки картинки на /pic и привязка эмодзи к пользователю

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-from pprint import pprint
+import logging
+from random import choice
 
+from emoji import emojize
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
-import logging
 
-from settings import PROXY, TOKEN
+from settings import PROXY, TOKEN, USER_EMOJI
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
@@ -15,16 +16,36 @@ log.addHandler(handler)
 
 
 def greet_user(update: Update, context: CallbackContext):
-    text = 'Вызван /start'
+    emo = get_user_emo(context.user_data)
+    text = f'Привет!{emo}'
     logging.info(text)
     update.message.reply_text(text)
 
 
+def send_pic(update: Update, context: CallbackContext):
+    context.bot.send_photo(chat_id=update.message.chat_id,
+                           photo='https://i.pinimg.com/736x/c7/12/43/c712434d2bf453f77513c0de26d3b4d1.jpg')
+
+
 def talk_to_me(update: Update, context: CallbackContext):
-    user_text = f'Привет, {update.message.chat.first_name}! Ты написал: {update.message.text}'
+    emo = get_user_emo(context.user_data)
+    user_text = f'Привет, {update.message.chat.first_name}! Ты написал: {update.message.text}{emo}'
     logging.info(f'User: {update.message.chat.username}, Chat_id: {update.message.chat_id}, '
                  f'Message: {update.message.text}')
     update.message.reply_text(user_text)
+
+
+def get_user_emo(user_data):
+    """
+    Проверяет, присвоен ли эмодзи пользователю
+    :param user_data: context.user_data
+    :return: user_data['emo']
+    """
+    if 'emo' in user_data:
+        return user_data['emo']
+    else:
+        user_data['emo'] = emojize(choice(USER_EMOJI), use_aliases=True)
+        return user_data['emo']
 
 
 def main():
@@ -34,6 +55,8 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('pic', send_pic))
+
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
